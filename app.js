b55522857b121593a8b1a0bbeaff5cdfd5244587
bennyh960 <bennyh960@gmail.com>
2022-06-16 00:52:33 +0300

    fixed cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 const express = require("express");
 // const chalk = require("chalk");
-const cors = require("cros");
+const cors = require("cors");
 const axios = require("axios");
 
 // ******************
